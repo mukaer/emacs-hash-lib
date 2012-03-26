076cdaa111d5416ebf5adbb900ff09138d79a35f
muaker <mukaer@gmail.com>
2012-03-27 05:20:04 +0900

    chg hash-lib.el
    add header
    change tab to spase

--- hash-lib.el
+++ hash-lib.el
@@ -1,3 +1,12 @@
+;;; hash-lib.el -- hash library
+
+;; Copyright (C) 2012
+;; (mukaer atmark gmail period com)
+
+;; This file is NOT a part of GNU Emacs.
+
+;;; Code:
+
 
 (defun list-to-hash (arg)
 
@@ -5,15 +14,15 @@
        ( j        (/ (length arg) 2)))
     (dotimes (i j)
       (let (( key (nth (* i 2)  arg))
-	    ( val (nth (+ 1 (* i 2)) arg)))
+            ( val (nth (+ 1 (* i 2)) arg)))
 
-	(if (typep val 'list)
-	    ;true
-	    (let ((reshash_sub (list-to-hash (car(cdr val))) ))
-	      (puthash (nth (* i 2)  arg) reshash_sub reshash))
-	  ;false
-	  (puthash key val  reshash))
-	)
+        (if (typep val 'list)
+            ;true
+            (let ((reshash_sub (list-to-hash (car(cdr val))) ))
+              (puthash (nth (* i 2)  arg) reshash_sub reshash))
+          ;false
+          (puthash key val  reshash))
+        )
       )
     reshash))
 
@@ -21,28 +30,28 @@
 (defun dump-hash (hash &optional indent)
   (let
       ((cnt (if indent
-		;true
-		(1+ indent)
-	      ;false
-	      0))
+                ;true
+                (1+ indent)
+              ;false
+              0))
        (hash hash))
 
     (with-temp-buffer
       (loop for key being the hash-keys of hash using (hash-values val)
-	    do
+            do
 
-	    (insert
-	     (make-string (* 8 cnt) ? )
-	     (format "%s => " key )
-	    (if  (typep val 'hash-table)
-		;true
-		(concat  "\n" (dump-hash val  cnt ) " \n")
-	      ;false
-	      (concat (format "%s ,\n" val )))
-	    )
+            (insert
+             (make-string (* 8 cnt) ? )
+             (format "%s => " key )
+            (if  (typep val 'hash-table)
+                ;true
+                (concat  "\n" (dump-hash val  cnt ) " \n")
+              ;false
+              (concat (format "%s ,\n" val )))
+            )
 
-	    finally (delete-backward-char 2)
-	    )
+            finally (delete-backward-char 2)
+            )
 
       (buffer-string)))
 )
@@ -61,33 +70,34 @@
 
 (defun get-hash (hash &rest rest)
   (let (
-	(first   (car rest) )
-	(second  (cdr rest) )
-	(arghash  hash)
-	)
+        (first   (car rest) )
+        (second  (cdr rest) )
+        (arghash  hash)
+        )
 
     (if second
-	;second true
-	(let ((reshash (gethash first arghash)))
-	  (if reshash
-	      (get-hash reshash  second )
-	    nil)
-	)
+        ;second true
+        (let ((reshash (gethash first arghash)))
+          (if reshash
+              (get-hash reshash  second )
+            nil)
+        )
       ;second false
       (if (typep first 'list)
-	  
-	  ;type first true
-	  (let ( (first (car first))
-		 (second (cdr first))
-		 (reshash (gethash  (car first) arghash))
-			)
-		 (if second
-		     (get-hash reshash  second)
-		   reshash
-	  ))
-	;type first false
-	(gethash first arghash)
+          
+          ;type first true
+          (let ( (first (car first))
+                 (second (cdr first))
+                 (reshash (gethash  (car first) arghash))
+                        )
+                 (if second
+                     (get-hash reshash  second)
+                   reshash
+          ))
+        ;type first false
+        (gethash first arghash)
       )
       )
 ))
 
+(provide 'hash-lib)
